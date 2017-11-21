badc817ae42e49bc76207df3293f9456073c5d97
Vitalii_Kozak <vitalii-kozak@ukr.net>
2017-11-21 14:24:45 +0200

    21.11.2017

--- src/main/java/calculator/service/CountingService.java
+++ src/main/java/calculator/service/CountingService.java
@@ -7,8 +7,12 @@ import calculator.domain.Service;
  * Created by Vitalii_Kozak on 11/20/2017.
  */
 public interface CountingService {
+    //Operation add two ComplexObject
     ComplexObject add(ComplexObject param1, ComplexObject param2);
+    //Operation subtract two ComplexObject
     ComplexObject subtract(ComplexObject param1, ComplexObject param2);
+    //Operation multiply two ComplexObject
     ComplexObject multiply(ComplexObject param1, ComplexObject param2);
+    //Operation divide two ComplexObject
     ComplexObject divide(ComplexObject param1, ComplexObject param2);
 }
--- src/main/java/calculator/service/StatisticService.java
+++ src/main/java/calculator/service/StatisticService.java
@@ -3,6 +3,8 @@ package calculator.service;
 import calculator.domain.Operation;
 
 public interface StatisticService {
+    ////Increase statistic by operation option
     void increase(Operation operation);
+    //Get statistic by operation option
     int getStatistic(Operation operation);
 }
